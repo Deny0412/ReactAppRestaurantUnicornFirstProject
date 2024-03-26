e3ca8b6d119c37b0587b4b8904e2d4afef0a5521
Deny0412 <denygrisa@gmail.com>
2024-03-26 18:44:42 +0100

    26.03.2024 V2

--- app-restaurant/server/abl/restaurace/update.js
+++ app-restaurant/server/abl/restaurace/update.js
@@ -1,30 +1,45 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 
 async function update(req, res) {
   try {
+    const id = parseInt(req.query.id); // Získání ID restaurace z query parametru
+
+    // Načtení seznamu restaurací z JSON souboru
     const restauraceList = JSON.parse(
-      await fs.promises.readFile(
-        path.join(__dirname, "../../storage/restaurace.json")
+      await fs.readFile(
+        path.join(__dirname, "../../storage/restaurace.json"),
+        "utf8"
       )
     );
-    const body = req.body;
-    const restauraceIndex = restauraceList.findIndex(
-      (restaurace) => restaurace.id === body.id
+
+    // Najít restauraci s daným ID
+    const restaurace = restauraceList.find(
+      (restaurace) => restaurace.id === id
     );
 
-    if (restauraceIndex > -1) {
-      restauraceList[restauraceIndex] = body;
+    if (restaurace) {
+      // Aktualizovat atributy restaurace podle poskytnutých parametrů
+      for (const key in req.query) {
+        if (key !== "id" && restaurace.hasOwnProperty(key)) {
+          restaurace[key] = req.query[key];
+        }
+      }
+
+      // Zápis aktualizovaných dat zpět do souboru
+      await fs.writeFile(
+        path.join(__dirname, "../../storage/restaurace.json"),
+        JSON.stringify(restauraceList, null, 2)
+      );
+
+      // Odpověď s úspěšně aktualizovanými daty restaurace
+      res.json(restaurace);
     } else {
+      // Pokud restaurace s daným ID neexistuje, vrátíme HTTP kód 404
       res.status(404).json({ message: "Restaurant not found" });
     }
-
-    await fs.promises.writeFile(
-      path.join(__dirname, "../../storage/restaurace.json"),
-      JSON.stringify(restauraceList, null, 2)
-    );
-    res.json(restauraceList[restauraceIndex]);
   } catch (e) {
+    // Pokud dojde k chybě při zpracování, vrátíme HTTP kód 500 s informací o chybě
     res.status(500).json({ message: e.message });
   }
 }
